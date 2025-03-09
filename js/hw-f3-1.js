5beef4db8668ed0e534f33acb46035fbf6035423
Vi. Sh. <vira.kyseliova@gmail.com>
2025-03-09 09:23:09 +0100

    hw-f3-1

---
+++ js/hw-f3-1.js
@@ -0,0 +1,28 @@
+const age = 16;
+console.log(age);
+
+const myName = "Vira";
+console.log(myName);
+
+const isStudent = true;
+console.log(isStudent);
+
+const myString = "Memento mori.";
+console.log(myString);
+
+let myNum = 11;
+console.log(myNum + 10);
+
+const myNull = null;
+console.log(myNull);
+
+const userName = prompt("Введіть своє ім’я");
+alert("Ви ввели: " + userName);
+console.log("Тип змінної userName:", typeof userName);
+
+const userAnswer = confirm("Ви дійсно хочете покинути сторінку?");
+console.log("Результат підтвердження:", userAnswer);
+
+alert("Увага! Дія може бути небезпечною.");
+const userConfirm = confirm("Ви впевнені, що хочете продовжити?");
+console.log("Користувач підтвердив дію:", userConfirm);
