fe714cfe3c6d9214d9bd47623c82c63f921696c4
Vi. Sh. <vira.kyseliova@gmail.com>
2025-03-23 12:05:56 +0100

    homework 2-4.5

---
+++ js/hw-f3-3.js
@@ -0,0 +1,56 @@
+let result;
+result = 5 + 5 + `5`;
+console.log(result);
+console.log("================================================");
+email = "test@gmail.com";
+console.log(email.includes("@"));
+console.log(email.length);
+console.log("================================================");
+test = "My";
+test2 = "name";
+test3 = "is";
+let fullName = test + test2 + test3 + " Viktor";
+console.log(fullName);
+console.log("================================================");
+let userName = "Абоба";
+let payment = 11;
+console.log(`Карочи ${userName} ты мне должен ${payment} гривен`);
+console.log("================================================");
+// const value1 = "";
+// const value2 = "";
+
+const value1 = "";
+const value2 = "df";
+
+// const value1 = "df";
+// const value2 = "df";
+
+if (value1 !== "" && value2 !== "") {
+  console.log("Обидва поля заповнені");
+} else {
+  console.log("Не всі поля заповнені");
+}
+console.log("================================================");
+// const num1 = 5;
+// const num2 = 5;
+
+// const num1 = 2;
+// const num2 = 5;
+
+const num1 = 11;
+const num2 = 5;
+
+if (num1 + num2 > 10) {
+  console.log("Сума більша за 10");
+} else {
+  console.log("Сума менша або дорівнює 10");
+}
+console.log("================================================");
+const js = "JavaScript";
+if (js.includes("JavaScript")) {
+  console.log("Текст містить слово JavaScript");
+} else {
+  console.log("Текст не містить слово JavaScript");
+}
+console.log("================================================");
+// Еще не доделанно, устала :(
